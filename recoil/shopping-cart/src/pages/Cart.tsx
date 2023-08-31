e59b145cc1419b6f376c225c829affe88d9d1046
ssm825 <ssm39825@gmail.com>
2023-08-31 11:08:34 +0900

    Feat : 장바구니 상품 개수, 최종 금액 추가

--- recoil/shopping-cart/src/pages/Cart.tsx
+++ recoil/shopping-cart/src/pages/Cart.tsx
@@ -1,21 +1,65 @@
 import CartItem from "components/CartItem/CartItem";
 import { useRecoilValue } from "recoil";
-import { cartAtom } from "recoil/CartAtom";
+import {
+  cartAtom,
+  totalPriceSelector,
+  totalQuantitySelector,
+} from "recoil/CartAtom";
 import styled from "styled-components";
 
 const Cart = () => {
   const cartItem = useRecoilValue(cartAtom);
+  const totalQuantity = useRecoilValue(totalQuantitySelector);
+  const totalPrice = useRecoilValue(totalPriceSelector);
   return (
-    <div>
-      {cartItem.length ? (
-        cartItem.map((cartData) => <CartItem data={cartData} />)
-      ) : (
-        <NoItems>장바구니가 비었습니다</NoItems>
-      )}
-    </div>
+    <>
+      <ItemWrapper>
+        {cartItem.length ? (
+          cartItem.map((cartData) => <CartItem data={cartData} />)
+        ) : (
+          <NoItems>장바구니가 비었습니다</NoItems>
+        )}
+      </ItemWrapper>
+      <TotalInfoWrapper>
+        <ColumnWrapper>
+          <span>상품 개수</span>
+          <Heading>{totalQuantity} 개</Heading>
+        </ColumnWrapper>
+        <ColumnWrapper>
+          <span>최종 결제금액</span>
+          <Heading>{totalPrice.toLocaleString()} 원</Heading>
+        </ColumnWrapper>
+      </TotalInfoWrapper>
+    </>
   );
 };
 
+const ItemWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: calc(100vh - 300px);
+`;
+const TotalInfoWrapper = styled.div`
+  padding: 16px;
+  width: 100%;
+  max-width: 1024px;
+  height: 150px;
+  border: 1px solid var(--line-gray);
+  & span {
+    text-align: right;
+  }
+`;
+const ColumnWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+`;
+const Heading = styled.span`
+  font-size: 20px;
+  font-weight: var(--bold);
+`;
 const NoItems = styled.div`
   margin: 0 auto;
   padding: 8px;
--- recoil/shopping-cart/src/recoil/CartAtom.ts
+++ recoil/shopping-cart/src/recoil/CartAtom.ts
@@ -1,7 +1,23 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 import { Product } from "./../types/product";
 
 export const cartAtom = atom<Product[]>({
   key: "cartAtom",
   default: [],
+});
+
+export const totalQuantitySelector = selector({
+  key: "totalQuantity",
+  get: ({ get }) => {
+    const quantity = get(cartAtom);
+    return quantity.length;
+  },
+});
+
+export const totalPriceSelector = selector({
+  key: "totalPrice",
+  get: ({ get }) => {
+    const price = get(cartAtom);
+    return price.reduce((acc, cur) => acc + cur.price, 0);
+  },
 });
